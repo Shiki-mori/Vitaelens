25bbed1c0e88a1c5db2a8abc8f7a51fa82eaa7d4
Shiki-mori <2296690003@qq.com>
2026-06-19 23:23:56 +0800

    完成前端vue项目创建

---
+++ vitaelens-backend/src/main/java/com/phrolova/vitaelensbackend/VitaelensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phrolova.vitaelensbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaelensBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitaelensBackendApplication.class, args);
+    }
+
+}
---
+++ vitaelens-backend/src/test/java/com/phrolova/vitaelensbackend/VitaelensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phrolova.vitaelensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaelensBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
